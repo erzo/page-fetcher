4f7771088a96608b145fa0c39301ab8d9aa826aa
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-02 04:32:03 +0000

    modified

--- fetcher.js
+++ fetcher.js
@@ -4,14 +4,23 @@ const request = require('request');
 
 
 request(input[0], (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code 
+  // console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code 
   //console.log('body:', body);
+
   fs.writeFile(input[1], body, (err) => {
-    if (err) throw err;
-    console.log('Replaced!');
-    // if (!input[1]) {
-    //   throw err;
-    // }
+    if (err) {
+      throw err;
+    };
+    console.log(`Downloaded and saved ${body.length} bytes to ${input[1]}`);
+    if (!input[1]) {
+      throw err;
+    }
   });
 });
+
+
+// returns true if input[1] file alreay exists
+// const dlTest = fs.access(input[1], fs.F_OK, (err) => {
+//   if (err) { throw err;}
+// });
